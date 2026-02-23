41a823a8fb4b33205a20cdefa80c053ba5926bcd
Juun <blacklily7513@gmail.com>
2026-02-23 11:06:07 +0900

    build: Organize package structure for plugin separation

---
+++ packages/typescript/tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  clean: true,
+  dts: true,
+  entry: ["src/index.ts"],
+  format: "cjs",
+  minify: true,
+  target: ["node22", "node24", "node25"],
+  external: ["@juun-roh/spine", "tree-sitter-typescript"],
+  sourcemap: true,
+});
