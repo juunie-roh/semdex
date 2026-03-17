258982216f5e97b13965bd4826cfb1625ed8cb7f
Juun <blacklily7513@gmail.com>
2026-03-17 12:00:10 +0900

    chore: Apply renamed class

--- __mocks__/plugin-module.ts
+++ __mocks__/plugin-module.ts
@@ -1,10 +1,10 @@
 import type TSParser from "tree-sitter";
 import { vi } from "vitest";
 
-import type Language from "@/core/language";
+import type LanguagePlugin from "@/core/language-plugin";
 
 const mockPlugin = vi.mockObject({
   language: vi.mockObject({} as TSParser.Language),
-} satisfies Language.Module);
+} satisfies LanguagePlugin.Module);
 
 const mockPlugin_no_language = vi.mockObject({});
